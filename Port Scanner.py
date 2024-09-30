1061c6202963543d3d98c0bdff11f696c7b0e5ba
Muhammad Bilal Badar <badarmuhammadbilal@gmail.com>
2024-09-30 16:28:11 +0500

    scanning the given port number on target IP

--- Port Scanner.py
+++ Port Scanner.py
@@ -1,8 +1,20 @@
 import pyfiglet
 import sys
 import socket
+import datetime 
 
 banner = pyfiglet.figlet_format("PORT SCANNER")
 print (banner)
 
-target= socket.gethostbyname(sys.argv[1])
+if len(sys.argv)==2:
+    target= socket.gethostbyname(sys.argv[1])
+else:
+    print("Invalid amount of argument, you have to enter the IP")
+
+port = int(input("\n Enter the port number you want to scan: "))
+print("Scanning Target: " + target)
+print("scanning started at: " + str(datetime.now()))
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.setdefaulttimeout(1)
+result = s.connect_ex((target,port))
