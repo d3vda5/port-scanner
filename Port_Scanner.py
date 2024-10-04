85de505f119c5a8878d00508217e3f614f12ecf2
Muhammad Bilal Badar <badarmuhammadbilal@gmail.com>
2024-10-05 01:58:31 +0500

    Error Handling

--- Port_Scanner.py
+++ Port_Scanner.py
@@ -14,14 +14,28 @@ else:
 print("-"*50)
 print("Scanning Target: " + target)
 print("scanning started at: " + str(datetime.now()))
+a= int(datetime.now())
 print("-"*50)
+try:
+    for port in range(0, 1023):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+        result = s.connect_ex((target,port))
+        if result == 0:
+            print(f"Port {port} is open")
+        s.close()
 
-for port in range(1, 1024):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socket.setdefaulttimeout(1)
-    result = s.connect_ex((target,port))
-    if result == 0:
-        print(f"Port {port} is open")
-    s.close
+except KeyboardInterrupt:
+    print("Quitting Program!!!!!!")
+    sys.exit()
+except socket.gaierror: 
+    print("\n Hostname Could Not Be Resolved !!!!") 
+    sys.exit() 
+except socket.error: 
+    print("\n Server not responding !!!!") 
+    sys.exit()
 
+print("-"*50)
+b=int(datetime.now)
 print("scanning ended at: " + str(datetime.now()))
+print(f"Total Scanning time: {b-a}")
